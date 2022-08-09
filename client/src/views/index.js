985fc30681d8652d0992c86587b3af42ee6ad886
yang_fu <1204914685@qq.com>
2022-08-09 18:22:44 +0800

    feat: index

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
+import './index.scss'
 
 export default function Home() {
   const navigate = useNavigate()
@@ -7,9 +7,16 @@ export default function Home() {
     navigate(`/home`)
   }
   return (
-    <div>
-       index
-       <div onClick={goHome}>enter</div>
+    <div className='container'>
+      <div className="panel-left">
+        <h1>Hello world</h1>
+        <p>If somebody somewhere has the right answer, suddenly you have it too. Collaborate better in a remote-first world.</p>
+        <div onClick={goHome} className="btn-primary">welcome</div>
+      </div>
+      <img src="./main.png" alt=""/>
+      <div class="ellips">
+        <div class="dot">T</div>
+      </div>
     </div>
   );
 }
